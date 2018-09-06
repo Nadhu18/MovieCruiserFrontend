492bb6a4c54955b11498a872537a63272a70808c
Gande.Rohith <gande.rohith@cognizant.com>
2018-09-06 13:46:24 +0000

    issues 3 fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -32,6 +32,7 @@ const appRoutes: Routes = [
     MatToolbarModule,
     MatButtonModule,
     MatInputModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot(appRoutes),
   ],
   providers: [],
