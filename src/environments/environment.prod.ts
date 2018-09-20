7997560e54da58104266fca1873f440011cdb398
Gande.Rohith <gande.rohith@cognizant.com>
2018-09-20 12:57:47 +0000

    dockerfile added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiKey: "api_key=e2101a3fd6939be53919aed858fac894",
   tmdbEndPoint: "https://api.themoviedb.org/3/movie",
   imagePrefix: "https://image.tmdb.org/t/p/w500/",
-  watchlistEndpoint: "http://localhost:49954/api/movie",
+  watchlistEndpoint: "http://localhost:8089/api/movie",
   searchlistEndpoint: "https://api.themoviedb.org/3/search/movie",
   movieDetailsEndpoint: "https://api.themoviedb.org/3/movie",
 };
